4aeeceaa5164958b575ffb257a4be788129f7e10
luancs <luan.soares.ext@infracommerce.com.br>
2022-11-04 00:40:32 -0300

    Created game class.

---
+++ src/entities/Jogo/Jogo.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "./Jogo.h"
+
+Jogo::Jogo(Jogador *jogador, Palavra *palavra, int numeroDeChances)
+{
+  this->jogador = jogador;
+  this->palavra = palavra;
+  this->chances = numeroDeChances;
+}
+
+Jogo::~Jogo()
+{
+  delete this->jogador;
+  delete this->palavra;
+}
+
+Jogador *Jogo::getJogador()
+{
+  return this->jogador;
+}
+
+Palavra *Jogo::getPalavra()
+{
+  return this->palavra;
+}
+
+int Jogo::getChances()
+{
+  return this->chances;
+}
+
+int Jogo::getRodada()
+{
+  return this->rodada;
+}
+
+void Jogo::setChances()
+{
+  if (this->chances > 0)
+    this->chances--;
+}
+
+void Jogo::setNovaRodada()
+{
+  this->rodada++;
+}
---
+++ src/entities/Jogo/Jogo.h
@@ -0,0 +1,27 @@
+#ifndef JOGO_H
+#define JOGO_H
+
+#include "../Jogador/Jogador.h"
+#include "../Palavra/Palavra.h"
+
+class Jogo
+{
+private:
+  Jogador *jogador;
+  Palavra *palavra;
+  int rodada;
+  int chances;
+  bool rodando;
+
+public:
+  Jogo(Jogador *, Palavra *, int);
+  ~Jogo();
+  Jogador *getJogador();
+  Palavra *getPalavra();
+  int getRodada();
+  int getChances();
+  void setChances();
+  void setNovaRodada();
+};
+
+#endif
